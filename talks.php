fc5831e435ef29e8ad54512ce99e91075c56127b
Lukas Kleinschmidt <kleinschmidt@eatersvz.de>
2017-06-25 10:34:37 +0200

    Update

--- talks.php
+++ talks.php
@@ -4,22 +4,17 @@ if(!function_exists('panel')) return;
 
 panel()->routes(array(
   array(
-    'pattern' => 'pages/talks/add',
+    'pattern' => 'pages/home/add',
     'action'  => function() {
 
       $controller = new Kirby\Panel\Controllers\Base();
-      $parent     = panel()->page('talks');
-      $self       = $this;
+      $parent     = panel()->page('home');
 
       if($parent->ui()->create() === false) {
         throw new PermissionsException();
       }
 
-      // somehow the reference to the form only works this way
-      // __DIR__ . DS . 'form.php' does not seem to work
-
-      $form = $parent->form(str_repeat('..' . DS, 4) . 'site' . DS . 'plugins' . DS . 'talks' . DS . 'form', function($form) use($parent, $self) {
-
+      $form = panel()->form(__DIR__ . DS . 'form.php', $parent, function($form) use($parent, $controller) {
         try {
 
           $form->validate();
@@ -33,17 +28,15 @@ panel()->routes(array(
             'title' => $data['title']
           ));
 
-          $self->notify(':)');
-          $this->redirect($page, 'edit');
+          $controller->notify(':)');
+          $controller->redirect($page, 'edit');
 
         } catch(Exception $e) {
           $form->alert($e->getMessage());
         }
       });
 
-      $content = new Brick('div');
-      $content->html($form);
-      $content->addClass('modal-content modal-content-medium');
+      $content = tpl::load(__DIR__ . DS . 'template.php', compact('form'));
 
       return $controller->layout('app', compact('content'));
     },
